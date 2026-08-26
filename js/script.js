7f0440366dc540baf0f42f3a6b2f681f7e3359c0
jose <joseluiscorreia157@gmail.com>
2026-08-26 15:59:07 -0300

    sintaxe

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 alert("Oi do externo");
+
+var num = parseFloat(prompt("digite um valor: "))
+if (num == 0){
+    alert(`${num} é zero`);
+}else if (num %2 != 0){
+    alert(`${num} é impar`);
+}else{
+    alert(`${num} é par`);
+}
+
+alert("Fim");
