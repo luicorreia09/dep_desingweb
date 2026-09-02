55e3ed9854d240381a6cdd7621a52e509acc7055
jose <joseluiscorreia157@gmail.com>
2026-09-02 15:31:56 -0300

    escopo e hoisting

--- js/script.js
+++ js/script.js
@@ -7,7 +7,19 @@ let altura;
 function calcIdade(ano = 2026){
     let idade = ano - nasc;
     console.log(`idade dentro da função: ${idade}`);
-    return idade;
+    let menor;      
+    if(idade < 18){
+         menor = true;
+    }else{
+         menor = false;
+    }
+    return [idade, menor];
+}
+let retorno = calcIdade()
+if (retorno[1]){
+    alert(`Idade: ${retorno[0]}. Você é menor de idade`);
+}else{
+    alert(`Idade: ${retorno[0]}. Você é maior de idade`);
 }
 alert(`idade fora da funcao: ${calcIdade()}`);
 
@@ -15,3 +27,4 @@ alert(`idade fora da funcao: ${calcIdade()}`);
 if (viva){
     altura = prompt("digite a sua altura");
 }
+          
---
+++ js/script2.js
@@ -0,0 +1,11 @@
+console.log("x",x);
+var x = 10;
+var x = 20;
+console.log("x",x);
+
+let y = 10;
+y = 20;
+console.log("y",y);
+
+const x = 20;
+console.log("z",z);
