eda699abf5d0f3fdb9fc8df8aafcf30507ead009
jose <joseluiscorreia157@gmail.com>
2026-09-09 14:35:55 -0300

    n semana

---
+++ js/script4.js
@@ -0,0 +1,18 @@
+let dia = prompt("sendo 1: Domingo e 7 Sábado, digite o dia da semana");
+if(dia == 1){
+    alert("hoje é domingo");
+}else if (dia == 2){
+    alert("hoje é segunda");
+}else if (dia == 3){
+    alert("hoje é terça");
+}else if (dia == 4){
+    alert("hoje é quarta");
+}else if (dia == 5){
+    alert("hoje é qinta");
+}else if (dia == 6){
+    alert("hoje é sexta");
+}else if (dia == 7){
+    alert("hoje é sábado");
+}else{
+    alert("Dia inválido");
+}
---
+++ js/script5.js
@@ -0,0 +1,11 @@
+let dia = Number(prompt("sendo 1: Domingo e 7 Sábado, digite o dia da semana"));
+switch(dia){
+    case 1: alert("Hoje é Domingo");break;
+    case 2: alert("Hoje é segunda ");break;
+    case 3: alert("hoje é terça");break;
+    case 4: alert("hoje é quarta");break;
+    case 5: alert("Hoje é  quinta");break;
+    case 6: alert("Hoje é sexta");break;
+    case 7: alert("Hoje é sábado");break;
+    default: alert("Dia inválido");break;
+}
