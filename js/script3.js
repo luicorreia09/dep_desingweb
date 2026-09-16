4c5dc914b65655d003b9b2582c91ad5964c6c1a1
jose <joseluiscorreia157@gmail.com>
2026-09-16 14:08:28 -0300

    funcao2.0

---
+++ js/script3.js
@@ -0,0 +1,13 @@
+let saudacao_a = function(){
+    alert(`Olá Fulano, seja bem-vindo!`);
+}
+
+let saudacao_b = function(nome){
+    alert(`Olá ${nome}, seja bem-vindo!`);
+}
+let saudacao_c = function(nome = "Desconhecido"){
+    alert(`Olá ${nome}, seja bem-vindo!`);
+}
+let saudacao_d = function(nome="Desconhecido"){
+    return `Olá ${nome}, seja Bem-Vindo!`;
+}
