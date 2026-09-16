aee5cc3d045094fa7f395726d85b9cf811684213
jose <joseluiscorreia157@gmail.com>
2026-09-16 13:59:14 -0300

    criando funçoes

---
+++ js/script2.js
@@ -0,0 +1,15 @@
+let saudacao_a = ()=>{
+    alert(`Olá Fulano, seja bem-vindo!`);
+}
+
+let saudacao_b = (nome)=>{
+    alert(`Olá ${nome}, seja bem-vindo!`);
+}
+
+let saudacao_c = (nome = "Desconhecido")=>{
+    alert(`Olá ${nome}, seja bem-vindo!`);
+}
+
+let saudacao_d = (nome = "Desconhecido")=>{
+    return `Olá ${nome}, seja bem-vindo!`;
+}
